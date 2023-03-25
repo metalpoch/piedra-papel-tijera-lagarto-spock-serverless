a02cbcb681b736b3f28edcd66d46d0a50ce03cbb
Keiber Urbila <33231797+metalpoch@users.noreply.github.com>
2023-03-25 18:51:24 -0400

    fix: Se elimino un comentario

--- utils/inputValidate.js
+++ utils/inputValidate.js
@@ -8,28 +8,7 @@ const getWinner = (player, pc) => {
   if (rules[player][pc]) return { winner: "Jugador", msg: rules[player][pc] };
   if (rules[pc][player]) return { winner: "PC", msg: rules[pc][player] };
 };
-/*
- *export default function (player) {
- *  const options = Object.keys(rules);
- *
- *  if (typeof player === "undefined") {
- *    const msg = "Se esperaba el parametro 'hand' con un valor"
- *    [>throw new Error(msg)<]
- *    return { data: null, error: { status: 400, msg } };
- *  }
- *  else if (!options.includes(player.toLowerCase())) {
- *    const msg = `Se esperaba una de las siguientes opciones: ${options.join(", ")}`;
- *    [>throw new Error(msg)<]
- *    return { data: null, error: { status: 400, msg } };
- *  }
- *  else {
- *    player = player.toLowerCase();
- *    const pc = randomValueFromArray(options);
- *    const { winner, msg } = getWinner(player, pc);
- *    return {error: null, data: { pc, player, winner, msg }};
- *  }
- *}
- **/
+
 export default function (player) {
   const options = Object.keys(rules);
 
