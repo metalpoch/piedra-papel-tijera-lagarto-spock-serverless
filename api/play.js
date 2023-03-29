e5b6e03a757cc77b64c2be429762c8e396f1ba22
Keiber Urbila <keiberup.dev@gmail.com>
2023-03-28 20:34:54 -0400

    feat: Se elimino startTransaction de sentry

--- api/play.js
+++ api/play.js
@@ -2,27 +2,13 @@ import inputValidate from "../utils/inputValidate.js";
 import Sentry from "../logs/sentry.js";
 
 export default function handler(req, res) {
-  const transaction = Sentry.startTransaction({
-    op: "test",
-    name: "Serverless /play endpoint",
-  });
-
-  if (req.method !== "GET") {
-    Sentry.captureMessage("Se esperaba el método GET");
+  if (req.method !== "GET")
     res.status(405).json({ error: "Se esperaba el método GET" });
-  }
-  else {
-    try {
-      const data = inputValidate(req.query.hand);
-      Sentry.captureMessage("serverless /api/play OK");
-      res.json(data);
-    }
-    catch (error) {
-      Sentry.captureException(error);
-      res.status(400).json({ error: error.message });
-    }
-    finally {
-      transaction.finish();
-    }
+  try {
+    const data = inputValidate(req.query.hand);
+    res.json(data);
+  } catch (error) {
+    Sentry.captureException(error);
+    res.status(400).json({ error: error.message });
   }
 }
